fe53105669e63e4bfd24d8d01c6d2da1fb722deb
João Alcântara <joaosantalcantara@gmail.com>
2021-12-14 15:10:06 -0300

    fix

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,4 +1,5 @@
-export default (req, res) => {
+/* eslint-disable import/no-anonymous-default-export */
+export default (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string }): any; new(): any } } }) => {
   return res.status(200).json({
     message: 'Hello world!'
   })
